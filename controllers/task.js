25d5e7c174a6a3b6f1c5457777d8d4ddbbf01eba
hulufei <f.calabash@gmail.com>
2014-02-21 16:04:49 +0800

    work base

---
+++ controllers/task.js
@@ -0,0 +1,33 @@
+var multiparty = require('multiparty');
+var T = require('t');
+var Task = require('../models/Task');
+
+exports.postTask = function(req, res, next) {
+  // Remove multipart middleware to handle upload files
+  // @see http://goo.gl/LC21f9
+  if (req.user && req.is('multipart/form-data')) {
+    var form = new multiparty.Form();
+    form.parse(req);
+
+    form.on('error', next);
+
+    form.on('part', function(part) {
+      // part is a readable stream
+      var t = new T(part);
+      t.parser.on('end', function(err) {
+        if (err) return next(err);
+
+        t.collections.forEach(function(c) { c.user = req.user; });
+
+        Task.create(t.collections, function(err) {
+          if (err) return next(err);
+
+          res.send(200);
+        });
+      });
+    });
+  }
+  else {
+    next();
+  }
+};
